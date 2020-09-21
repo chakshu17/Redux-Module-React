be3e3dc369d41e16e0d26b0926cbc2fc6f50e8d2
Chakshu kishnani <chakshu05kishnani@gmail.com>
2020-09-21 12:56:08 +0530

    create and providing store to app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import './App.css';
 import CakeContainer from './components/CakeContainer';
+import store from './redux/store'
 
 function App() {
   return (
-    <div className="App" style={{color: 'balck',background:'white'}}>
+    <Provider store={store}>
+      <div className="App" style={{color: 'balck',background:'white'}}>
       Redux Module
       <div>
         <CakeContainer/>
       </div>
     </div>
+    </Provider>
   );
 }
 
