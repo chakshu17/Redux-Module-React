3dd6c651faa25c999f9fe9cb409518f39968377a
Chakshu kishnani <chakshu05kishnani@gmail.com>
2020-09-25 22:49:33 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import './App.css';
+import React from "react";
+import { Provider } from "react-redux";
+import "./App.css";
 
-import UserContainer from './components/UserContainer';
-import store from './redux/store'
+import UserContainer from "./components/UserContainer";
+import store from "./redux/store";
 
 function App() {
-  return (
-    <Provider store={store}>
-      <div className="App" style={{color: 'balck',background:'white'}}>
-      Redux Module
-      <div>
-        <UserContainer/>
-        {/* <ItemContainer cake/>
+	return (
+		<Provider store={store}>
+			<div className="App" style={{ color: "balck", background: "white" }}>
+				Redux Module
+				<div>
+					<UserContainer />
+					{/* <ItemContainer cake/>
         <ItemContainer/>
         <CakeContainer/>
         <HooksCakeContainer/>
         <IceCreamContainer/>
         <NewCakeContainer/> */}
-      </div>
-    </div>
-    </Provider>
-  );
+				</div>
+			</div>
+		</Provider>
+	);
 }
 
 export default App;
